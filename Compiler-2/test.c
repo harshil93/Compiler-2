f5363da5197e4f69c81004831f8ad7634dd508ab
shobhit6993 <sc.shobhit@gmail.com>
2014-02-08 09:29:23 +0530

    added flex part

---
+++ Compiler-2/test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+	int a, be2w;
+	a = a + 2;
+	if (a <= = 3)
+	{
+		printf("laksdjflsjadf\n");
+		char in = 'a';
+	}
+	b = b << a;
+	!a&b,
+		return 0;		//dslfjlk sdlfkj
+	/*kdklfjjsd
+	sdlkjlksjdf
+
+	*/
+
+
+}
+
+#define     
+dslkafj
